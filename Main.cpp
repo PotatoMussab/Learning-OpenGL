e84cce7c9279755d82ce97fb3af4088adeb2df82
PotatoMussab <mdarhoubi@gmail.com>
2026-04-22 16:17:33 +0200

    Small cleanup

--- Main.cpp
+++ Main.cpp
@@ -7,75 +7,67 @@
 const unsigned int WIN_WIDTH = 800;
 const unsigned int WIN_HEIGHT = 600;
 const char *WIN_TITLE = "Learning OpenGL";
-const char *vertexShaderSourceCode =
-    "#version 330 core\n"
-    "layout (location=0) in vec3 inPos;\n"
-    "layout (location=1) in vec3 inColor;\n"
-    "out vec3 vertexColor;\n"
-    "void main()\n"
-    "{\n"
-    "gl_Position = vec4(inPos, 1.0f);\n"
-    "vertexColor = inColor;\n"
-    "}\0";
-const char *fragmentShaderPath = ".\\shaders\\shader.fs";
+const char *VERT_SHADER_PATH = ".\\shaders\\shader.vs";
+const char *FRAG_SHADER_PATH = ".\\shaders\\shader.fs";
 //---------------------------Prototype functions-----------------------------------------
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void keyPress_callback(GLFWwindow *window, int key, int scancode, int action, int mods);
-GLuint createShader(GLenum shaderType, const char *const *pSourceCode); //Pass pointer to a string
+GLuint createShader(GLenum shaderType, const char *const *pSourceCode); // Pass pointer to a string
 GLuint createShaderFromFile(GLenum shaderType, const char *const *filePath);
 //--------------------------------Main---------------------------------------------------
 int main()
 {
-    if(!glfwInit()) //Initialize GLFW
+    if (!glfwInit()) // Initialize GLFW
     {
         std::cout << "Failed to initialize GLFW" << std::endl;
         return -1;
     }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR,3); //Set version to OpenGL 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR,3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);// Set to core profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // Set version to OpenGL 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // Set to core profile
 
-    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL); //Create window
-    if (window == NULL) //In case window doesn't create
+    GLFWwindow *window = glfwCreateWindow(WIN_WIDTH, WIN_HEIGHT, WIN_TITLE, NULL, NULL); // Create window
+    if (window == NULL)                                                                  // In case window doesn't create
     {
         std::cout << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
-    glfwMakeContextCurrent(window); //Set the context to the created window
+    glfwMakeContextCurrent(window); // Set the context to the created window
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) //Load GLAD (needs an existing OpenGL context beforehand)
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) // Load GLAD (needs an existing OpenGL context beforehand)
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    glViewport(0,0, 800,600); //Create a viewport for the window. Maps from (-1,1) to (0, max width/height)
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); //Set callback for resizing window
-    glfwSetKeyCallback(window, keyPress_callback); //Set callback for key press
+    glViewport(0, 0, 800, 600);                                        // Create a viewport for the window. Maps from (-1,1) to (0, max width/height)
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); // Set callback for resizing window
+    glfwSetKeyCallback(window, keyPress_callback);                     // Set callback for key press
 
     float triangleVertices[] = {
-         0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right 
+        0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // bottom right
         -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // bottom left
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f // top 
+        0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f    // top
     };
 
     GLuint vertexShader = createShader(GL_VERTEX_SHADER, &vertexShaderSourceCode);
-    GLuint fragmentShader = createShaderFromFile(GL_FRAGMENT_SHADER, &fragmentShaderPath);
-    //Create shader program
+    GLuint fragmentShader = createShaderFromFile(GL_FRAGMENT_SHADER, &FRAG_SHADER_PATH);
+    // Create shader program
     GLuint shaderProgram = glCreateProgram();
-    //Attach shaders to program
+    // Attach shaders to program
     glAttachShader(shaderProgram, vertexShader);
     glAttachShader(shaderProgram, fragmentShader);
     glLinkProgram(shaderProgram);
-    //Check for linking errors
+    // Check for linking errors
     int success;
     char infoLog[512];
     glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if(!success)
+    if (!success)
     {
         glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR LINKING SHADERS\n" << infoLog << std::endl;
+        std::cout << "ERROR LINKING SHADERS\n"
+                  << infoLog << std::endl;
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
@@ -88,51 +80,52 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(triangleVertices), triangleVertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*)0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6*sizeof(float), (void*) (3*sizeof(float)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(0);
     glEnableVertexAttribArray(1);
 
     glUseProgram(shaderProgram);
-    glClearColor(0.2f, 0.2f, 0.3f, 1.0f); //Set color (RGBA color scheme) for clearing window
+    glClearColor(0.2f, 0.2f, 0.3f, 1.0f); // Set color (RGBA color scheme) for clearing window
 
-    while(!glfwWindowShouldClose(window)) //While window shouldn't close
+    while (!glfwWindowShouldClose(window)) // While window shouldn't close
     {
-        glClear(GL_COLOR_BUFFER_BIT); //Clear color buffer before rendering next frame
+        glClear(GL_COLOR_BUFFER_BIT); // Clear color buffer before rendering next frame
         glDrawArrays(GL_TRIANGLES, 0, 3);
-        glfwSwapBuffers(window); //Swap color buffers (render next frame)
-        glfwPollEvents(); //Check for events
+        glfwSwapBuffers(window); // Swap color buffers (render next frame)
+        glfwPollEvents();        // Check for events
     }
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteProgram(shaderProgram);
 
-    glfwTerminate(); //Close window
+    glfwTerminate(); // Close window
     return 0;
 }
 
 //------------------------------------------Functions-----------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) { glViewport(0,0, width, height); }
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) { glViewport(0, 0, width, height); }
 void keyPress_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 GLuint createShader(GLenum shaderType, const char *const *pSourceCode)
 {
-    //Create vertex shader
+    // Create vertex shader
     GLuint shader = glCreateShader(shaderType);
     glShaderSource(shader, 1, pSourceCode, NULL);
     glCompileShader(shader);
-    //Check for compilation errors
+    // Check for compilation errors
     int success;
     char infoLog[512];
     glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
-    if(!success)
+    if (!success)
     {
         glGetShaderInfoLog(shader, 512, NULL, infoLog);
-        std::cout << "ERROR COMPILING SHADER\n" << infoLog << std::endl;
+        std::cout << "ERROR COMPILING SHADER\n"
+                  << infoLog << std::endl;
     }
     return shader;
 }
@@ -140,7 +133,7 @@ GLuint createShaderFromFile(GLenum shaderType, const char *const *filePath)
 {
     GLuint shader = 0;
     std::ifstream file;
-    file.exceptions (std::ifstream::failbit | std::ifstream::badbit);
+    file.exceptions(std::ifstream::failbit | std::ifstream::badbit);
     try
     {
         file.open(*filePath);
@@ -148,10 +141,10 @@ GLuint createShaderFromFile(GLenum shaderType, const char *const *filePath)
         fileStream << file.rdbuf();
         file.close();
         std::string fileContent = fileStream.str();
-        const char* sourceCode = fileContent.c_str();
+        const char *sourceCode = fileContent.c_str();
         shader = createShader(shaderType, &sourceCode);
     }
-    catch(std::ifstream::failure e)
+    catch (std::ifstream::failure e)
     {
         std::cout << "ERROR READING SHADER FILE" << std::endl;
     }
---
+++ shaders/shader.vs
@@ -0,0 +1,9 @@
+#version 330 core
+layout (location=0) in vec3 inPos;
+layout (location=1) in vec3 inColor;
+out vec3 vertexColor;
+void main()
+{
+gl_Position = vec4(inPos, 1.0f);
+vertexColor = inColor;
+}
