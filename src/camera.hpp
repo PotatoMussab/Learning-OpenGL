14ba754bb72f8cf31f24f8f18bac408f89eccdbb
PotatoMussab <mdarhoubi@gmail.com>
2026-04-30 18:31:09 +0200

    Make Camera direction and up private to ensure they always remain normalized

--- src/camera.hpp
+++ src/camera.hpp
@@ -6,8 +6,11 @@ using namespace glm;
 class Camera
 {
     public:
-        vec3 pos, up, dir;
+        vec3 pos;
+        float pitch, yaw; //Pitch is up/down, yaw is left right
         float speed, turnRate;
+
+        //Constructors
         Camera()
         {
             pos = vec3(0.0f, 0.0f, -1.0f);
@@ -15,28 +18,46 @@ class Camera
             dir = vec3(0.0f, 0.0f, 1.0f);
             speed = 1.0f;
         }
-        Camera(vec3 setPos, vec3 setUp, vec3 setDir)
+        Camera(vec3 setPos, vec3 newUp, vec3 newDir)
         {
             pos = setPos;
-            up = setUp;
-            dir = setDir;
+            up = glm::normalize(newUp);
+            dir = glm::normalize(newDir);
         }
+
+        //Set and get functions
+        inline vec3 getDir()
+            {return dir;}
+        inline vec3 getUp()
+            {return up;}
+        inline void setDir(vec3 newDir)
+            {dir = normalize(newDir);}
+        inline void setUp(vec3 newUp)
+            {up = normalize(newUp);}
+
+        //Camera movement functions
         void moveForward(float dt)
-            {pos = pos + speed * dt * normalize(dir);}
+            {pos = pos + speed * dt * dir;}
         void moveBackward(float dt)
-            {pos = pos - speed * dt * normalize(dir);}
+            {pos = pos - speed * dt * dir;}
         void moveUp(float dt)
-            {pos = pos + speed * dt * normalize(up);}
+            {pos = pos + speed * dt * up;}
         void moveDown(float dt)
-            {pos = pos - speed * dt * normalize(up);}
+            {pos = pos - speed * dt * up;}
         void strafeRight(float dt)
         {
-            vec3 rightDir = normalize(cross(dir,up));
+            vec3 rightDir = cross(dir,up);
             pos = pos + speed * dt * rightDir;
         }
         void strafeLeft(float dt)
         {
-            vec3 leftDir = -normalize(cross(dir,up));
+            vec3 leftDir = -cross(dir,up);
             pos = pos + speed * dt * leftDir;
         }
+        void turn(float dx, float dy, float dt)
+        {
+            
+        }
+    private:
+        vec3  up, dir;
 };
